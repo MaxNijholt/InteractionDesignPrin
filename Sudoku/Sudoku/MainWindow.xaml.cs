af669a2f676f390d4ce6a9ace24e1791fa5df7d9
Max Nijholt <m.nijholt@student.avans.nl>
2015-01-14 19:24:01 +0100

    Initial Commit

---
+++ Sudoku/Sudoku/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SudokuBasic;
+using System.Data;
+
+namespace Sudoku {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow ( ) {
+			InitializeComponent( );
+			Wrapper c = new Wrapper();
+			c.Create( );
+			DataTable table = new DataTable( );
+
+			for ( int y = 0; y < 9; y++ ) {
+				DataRow dr = table.NewRow( );
+				for ( int x = 0; x < 9; x++ ) {
+					string temp = ""+c.Get( (short) x, (short) y );
+					dr[x] = x;
+				}
+				table.Rows.Add( dr );
+			}
+
+			datagrid.DataContext = table;
+		}
+	}
+}
---
+++ Sudoku/SudokuBasic/Wrapper.cs
@@ -0,0 +1,45 @@
+﻿namespace SudokuBasic {
+	public class Wrapper {
+		public Sudoku.IGame myGame = new Sudoku.Game( );
+		public Wrapper ( ) {
+		}
+
+		public void Create ( ) {
+			myGame.create( );
+		}
+
+		public bool Set ( short x, short y, short value ) {
+			int lel;
+			myGame.set( x, y, value, out lel );
+			return lel == 1;
+		}
+
+		public short Get ( short x, short y ) {
+			short val;
+			myGame.get( x, y, out val );
+			return val;
+		}
+
+		public bool IsValid ( ) {
+			int valid;
+			myGame.isValid(out valid);
+			return valid == 1;
+		}
+
+		public bool Read ( ) {
+			int done;
+			myGame.read( out done );
+			return done == 1;
+		}
+
+		public bool Write ( ) {
+			int write;
+			myGame.write( out write );
+			return write == 1;
+		}
+
+		public void Hint (out short x, out short y, out short val, out int succeeded) {
+			myGame.hint( out x, out y, out val, out succeeded );
+		}
+	}
+}
